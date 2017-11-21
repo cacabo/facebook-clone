131275dcca4c304a9920be11416cfbcd26c4d136
ccabo1 <ccabo@seas.upenn.edu>
2017-11-21 09:26:30 -0500

    login form

--- frontend/components/users/Login.js
+++ frontend/components/users/Login.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import Thin from '../shared/Thin';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    return (
-        <div>
-          <h1>Login</h1>
-          <p>
-            Log in here
-          </p>
-        </div>
-    );
+  return (
+    <Thin>
+      <div className="card">
+        <h3 className="bold marg-bot-1">
+          Login
+        </h3>
+        <form className="line-form">
+          <label>
+            Email
+          </label>
+          <input type="email" name="email" className="form-control marg-bot-1" />
+
+          <label>
+            Password
+          </label>
+          <input type="password" name="password" className="form-control marg-bot-1" />
+
+          <input type="submit" className="btn btn-primary full-width cursor" value="Create account" />
+        </form>
+        <p className="marg-top-1 marg-bot-0">
+          Don't have an account? <Link to="/register" className="inline">create one here.</Link>
+        </p>
+      </div>
+    </Thin>
+  );
 };
 
 export default Login;
