e8f1d65cccc5b5e5b61aa6978fd2197df7f4e15e
ccabo1 <ccabo@seas.upenn.edu>
2017-11-22 02:05:29 -0500

    Basic API stuff

--- backend/database.js
+++ backend/database.js
@@ -4,3 +4,26 @@ var keyvaluestore = require('./keyvaluestore.js');
 // Create the usersTable
 var users = new keyvaluestore('usersTable');
 users.init(function(err, data){});
+
+// Get a user with the specified ID
+var get_user = function(username, callback) {
+  users.get(username, function(err, data) {
+    if (err || !data) {
+      // If there was an issue getting the data
+      callback(null, "User not found: " + err);
+    } else {
+      // Find the value in the returned data
+      var value = JSON.parse(data[0].value);
+
+      // Return the value without error
+      callback(value, null);
+    }
+  });
+}
+
+// Create the database object to export
+const database = {
+  getUser: get_user,
+};
+
+module.exports = database;
--- backend/routes.js
+++ backend/routes.js
@@ -4,10 +4,19 @@ const db = require('./database.js');
 
 // API routes
 router.get('/users', (req, res) => {
-    res.send({
-      location: "/users",
-      success: true,
-    });
+  db.getUser("cameroncabo@gmail.com", (data, err) => {
+    if (err || !data) {
+      res.send({
+        success: false,
+        err: err,
+      });
+    } else {
+      res.send({
+        success: true,
+        data: data,
+      });
+    }
+  });
 });
 
 module.exports = router;
