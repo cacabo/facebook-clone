17908df54bf266b86fb3f3d9aba5b7d5c52e367b
ccabo1 <ccabo@seas.upenn.edu>
2017-11-21 20:23:39 -0500

    Styling information about user

--- frontend/components/users/Profile.js
+++ frontend/components/users/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Medium from '../shared/Medium';
 import StatusForm from '../newsfeed/StatusForm';
+import Status from '../newsfeed/Status';
 
 const Profile = () => {
   return (
@@ -16,24 +17,45 @@ const Profile = () => {
             <div className="profile-picture"></div>
 
             <div className="row">
-              <div className="col-12 col-md-4">
-                <div className="space-4"></div>
-                <h4>
-                  More about me
-                </h4>
+              <div className="col-12 col-md-4 about">
+                <strong>
+                  Learn more
+                </strong>
                 <p>
-                  This is some cool stuff
+                  This is my biography
+                </p>
+                <strong>
+                  Interests
+                </strong>
+                <ul className="tags">
+                  <li>NETS 212</li>
+                  <li>Scalable cloud computing</li>
+                  <li>Computer science</li>
+                </ul>
+                <p>
+                  <strong>Friends:</strong> 212
+                </p>
+                <p>
+                  <strong>Posts:</strong> 41
                 </p>
               </div>
               <div className="col-12 col-md-8">
                 <StatusForm placeholder="Write on this user's wall" />
 
-                <div className="card">
-                  <h1>User profile</h1>
-                  <p>
-                    This is an example user profile
-                  </p>
-                </div>
+                <Status
+                  name="Terry Jo"
+                  status="I'm a fool loool"
+                  userImg="https://scontent-lga3-1.xx.fbcdn.net/v/t31.0-8/15585239_1133593586737791_6146771975815537560_o.jpg?oh=1f5bfe8e714b99b823263e2db7fa3329&oe=5A88DA92"
+                  id="1"
+                />
+
+                <Status
+                  name="Terry Jo"
+                  status="Look at this dog"
+                  userImg="https://scontent-lga3-1.xx.fbcdn.net/v/t31.0-8/15585239_1133593586737791_6146771975815537560_o.jpg?oh=1f5bfe8e714b99b823263e2db7fa3329&oe=5A88DA92"
+                  id="1"
+                  image="http://www.insidedogsworld.com/wp-content/uploads/2016/03/Dog-Pictures.jpg"
+                />
               </div>
             </div>
           </div>
