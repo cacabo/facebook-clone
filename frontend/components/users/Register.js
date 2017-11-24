b475190b52031d87ecd6e45147132c1d0ffc0de1
ccabo1 <ccabo@seas.upenn.edu>
2017-11-24 18:37:05 -0500

    Stateful registration

--- frontend/components/users/Register.js
+++ frontend/components/users/Register.js
@@ -5,52 +5,154 @@ import { Link } from 'react-router-dom';
 /**
  * Component to render a form to register a user
  */
-const Register = () => {
-  return (
-    <Thin>
-      <div className="card">
-        <h3 className="marg-bot-1 bold">
-          Register
-        </h3>
-        <form className="line-form">
-          <label>
-            Email
-          </label>
-          <input type="email" name="email" className="form-control marg-bot-1" autoFocus="true" />
+class Register extends React.Component {
+  // Constructor method
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      firstName: "",
+      lastName: "",
+      password: "",
+      confirmPassword: "",
+    };
 
-          <div className="row">
-            <div className="col-6">
-              <label>
-                First name
-              </label>
-              <input type="text" name="firstName" className="form-control marg-bot-1" />
+    // Binding "this" to each state helper method
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
+    this.handleChangeLastName = this.handleChangeLastName.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangeConfirmPassword = this.handleChangeConfirmPassword.bind(this);
+  }
+
+  // Handle a change to the email state
+  handleChangeEmail(event) {
+    this.setState({
+      email: event.target.value,
+    });
+  }
+
+  // Handle a change to the first name state
+  handleChangeFirstName(event) {
+    this.setState({
+      firstName: event.target.value,
+    });
+  }
+
+  // Handle a change to the last name state
+  handleChangeLastName(event) {
+    this.setState({
+      lastName: event.target.value,
+    });
+  }
+
+  // Handle a change to the password state
+  handleChangePassword(event) {
+    this.setState({
+      password: event.target.value,
+    });
+  }
+
+  // Handle a change to the confirm password state
+  handleChangeConfirmPassword(event) {
+    this.setState({
+      confirmPassword: event.target.value,
+    });
+  }
+
+  // Render the component
+  render() {
+    return (
+      <Thin>
+        <div className="card">
+          <h3 className="marg-bot-1 bold">
+            Register
+          </h3>
+          <form className="line-form">
+            <label>
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              className="form-control marg-bot-1"
+              value={ this.state.email }
+              onChange={ this.handleChangeEmail }
+              autoFocus="true"
+            />
+
+            <div className="row">
+              <div className="col-6">
+                <label>
+                  First name
+                </label>
+                <input
+                  type="text"
+                  name="firstName"
+                  className="form-control marg-bot-1"
+                  value={ this.state.firstName }
+                  onChange={ this.handleChangeFirstName }
+                />
+              </div>
+              <div className="col-6">
+                <label>
+                  Last name
+                </label>
+                <input
+                  type="text"
+                  name="lastName"
+                  className="form-control marg-bot-1"
+                  value={ this.state.lastName }
+                  onChange={ this.handleChangeLastName }
+                />
+              </div>
             </div>
-            <div className="col-6">
-              <label>
-                Last name
-              </label>
-              <input type="text" name="lastName" className="form-control marg-bot-1" />
-            </div>
-          </div>
 
-          <label>
-            Password
-          </label>
-          <input type="password" name="password" className="form-control marg-bot-1" />
+            <label>
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              className="form-control marg-bot-1"
+              value={ this.state.password }
+              onChange={ this.handleChangePassword }
+            />
 
-          <label>
-            Confirm password
-          </label>
-          <input type="password" name="confirmPassword" className="form-control marg-bot-1" />
+            <label>
+              Confirm password
+            </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              className="form-control marg-bot-1"
+              value={ this.state.confirmPassword }
+              onChange={ this.handleChangeConfirmPassword }
+            />
 
-          <input type="submit" className="btn btn-primary full-width cursor" value="Create account" />
-        </form>
-        <p className="marg-top-1 marg-bot-0">
-          Already have an account? <Link to="/login" className="inline">login here.</Link>
-        </p>
-      </div>
-    </Thin>
-  );
+            <input
+              type="submit"
+              className={
+                (
+                  this.state.email &&
+                  this.state.firstName &&
+                  this.state.lastName &&
+                  this.state.password &&
+                  this.state.confirmPassword
+                ) ?
+                "btn btn-primary full-width cursor" :
+                "btn btn-primary full-width disabled"
+              }
+              value="Create account"
+            />
+          </form>
+          <p className="marg-top-1 marg-bot-0">
+            Already have an account? <Link to="/login" className="inline">login here.</Link>
+          </p>
+        </div>
+      </Thin>
+    );
+  }
 };
 
 export default Register;
