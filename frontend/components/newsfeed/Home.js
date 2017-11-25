3f119e37fc07572c0852dd9cef46e2b56c7c3dec
ccabo1 <ccabo@seas.upenn.edu>
2017-11-25 10:24:52 -0500

    Stateful homepage

--- frontend/components/newsfeed/Home.js
+++ frontend/components/newsfeed/Home.js
@@ -3,6 +3,7 @@ import StatusForm from './StatusForm';
 import Status from './Status';
 import FriendRecommendations from './FriendRecommendations';
 import OnlineNow from './OnlineNow';
+import uuid from 'uuid-v4';
 
 /**
  * Component to render a user's newsfeed.
@@ -11,37 +12,71 @@ import OnlineNow from './OnlineNow';
  * however, a user sees 1. recommended friends (to the left), 2. their newsfeed,
  * 3. a list of friends currently online.
  */
-const Home = () => {
-  return (
-    <div className="container-fluid marg-top-1">
-      <div className="row">
-        <div className="col-lg-3 hidden-md-down">
-          <FriendRecommendations />
-        </div>
-        <div className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-0 col-xl-5">
-          <StatusForm placeholder="What's on your mind?" />
+class Home extends React.Component {
+  // Constructor method
+  constructor(props) {
+    super(props);
 
-          <Status
-            name="Terry Jo"
-            status="I'm a fool loool"
-            userImg="https://scontent-lga3-1.xx.fbcdn.net/v/t31.0-8/15585239_1133593586737791_6146771975815537560_o.jpg?oh=1f5bfe8e714b99b823263e2db7fa3329&oe=5A88DA92"
-            id="1"
-          />
+    /**
+     * TODO REPLACE THIS DUMMY DATA
+     */
+    this.state = {
+      statuses: [
+        {
+          name: "Terry Jo",
+          status: "I'm a fool loool",
+          userImg: "https://scontent-lga3-1.xx.fbcdn.net/v/t31.0-8/15585239_1133593586737791_6146771975815537560_o.jpg?oh=1f5bfe8e714b99b823263e2db7fa3329&oe=5A88DA92",
+          id: "1",
+        },
+        {
+          name: "Terry Jo",
+          status: "Look at this dog",
+          userImg: "https://scontent-lga3-1.xx.fbcdn.net/v/t31.0-8/15585239_1133593586737791_6146771975815537560_o.jpg?oh=1f5bfe8e714b99b823263e2db7fa3329&oe=5A88DA92",
+          id: "1",
+          userImg: "https://scontent-lga3-1.xx.fbcdn.net/v/t31.0-8/15585239_1133593586737791_6146771975815537560_o.jpg?oh=1f5bfe8e714b99b823263e2db7fa3329&oe=5A88DA92",
+        },
+      ],
+    }
+  }
 
-          <Status
-            name="Terry Jo"
-            status="Look at this dog"
-            userImg="https://scontent-lga3-1.xx.fbcdn.net/v/t31.0-8/15585239_1133593586737791_6146771975815537560_o.jpg?oh=1f5bfe8e714b99b823263e2db7fa3329&oe=5A88DA92"
-            id="1"
-            image="http://www.insidedogsworld.com/wp-content/uploads/2016/03/Dog-Pictures.jpg"
-          />
-        </div>
-        <div className="col-md-3 col-xl-4 hidden-md-down">
-          <OnlineNow />
+  // Helper function to render statuses on the homepage
+  // This renders the statuses contained in the state of the component
+  renderStatuses() {
+    return this.state.statuses.map((status) => {
+      return (
+        <Status
+          name={ status.name }
+          status={ status.status }
+          userImg={ status.userImg }
+          img={ status.img }
+          id={ status.id }
+          key={ uuid() }
+        />
+      )
+    });
+  }
+
+  // Render the component
+  render() {
+    return (
+      <div className="container-fluid marg-top-1">
+        <div className="row">
+          <div className="col-lg-3 hidden-md-down">
+            <FriendRecommendations />
+          </div>
+          <div className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-0 col-xl-5">
+            <StatusForm placeholder="What's on your mind?" />
+
+            { this.renderStatuses() }
+
+          </div>
+          <div className="col-md-3 col-xl-4 hidden-md-down">
+            <OnlineNow />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Home;
