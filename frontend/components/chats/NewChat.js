5086064aff50924d09d212531f9d898b9cdadedb
ccabo1 <ccabo@seas.upenn.edu>
2017-11-26 11:44:31 -0500

    working on chat form

--- frontend/components/chats/NewChat.js
+++ frontend/components/chats/NewChat.js
@@ -1,16 +1,14 @@
 import React from 'react';
+import Chats from './Chats';
 
 /**
  * Component to render one of a user's group chats.
  */
 const NewChat = () => {
   return (
-    <div>
+    <Chats>
       <h1>This is a form for a new chat</h1>
-      <p>
-        This is a chat
-      </p>
-    </div>
+    </Chats>
   );
 };
 
