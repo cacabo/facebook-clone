95a9909a8732905945f0d8c412db46300110b280
terrencejo <terryinsungjo@gmail.com>
2017-12-10 12:26:24 -0500

    fixed error checking

--- backend/likes.js
+++ backend/likes.js
@@ -8,8 +8,12 @@ const uuid = require('uuid-v4');
  */
 function checkLike(liker, statusUser, statusID, callback) {
   // Check if either liker or status is null
-  if (!liker || !statusID) {
-    callback(null, "Either the liker or status is null.");
+  if (!liker) {
+    callback(null, "User must be logged in to like a status.");
+  } else if (!statusUser) {
+    callback(null, "Status user is undefined.");
+  } else if (!statusID) {
+    callback(null, "StatusID is undefined.");
   } else {
     // Query for like. If like exists, return data, if not return null;
     Like
@@ -34,64 +38,60 @@ function checkLike(liker, statusUser, statusID, callback) {
  * Adds a Like object to the likesTable and updates the likeCount in the status
  */
 function addLike(liker, statusUser, statusID, callback) {
-  // Error checking
-  if (!liker || !statusUser || !statusID) {
-    callback(null, "Liker, status, and statusID must be populated.");
-  } else {
-    // Check that the status exists, query using statusID
-    Status
-      .query(statusUser)
-      .where('id').equals(statusID)
-      .exec((err, data) => {
-        // Status is not found or there is an error
-        if (err || !data) {
-          if (err) {
-            callback(null, "There was an error looking up status: " + err);
-          } else {
-            callback(null, "Status does not exist.");
-          }
+  // Query for the status
+  Status
+    .query(statusUser)
+    .where('id').equals(statusID)
+    .exec((err, data) => {
+      // Status is not found or there is an error
+      if (err || !data) {
+        if (err) {
+          callback(null, "There was an error looking up status: " + err);
         } else {
-          // Status object that is being liked
-          const statusObject = data.Items[0].attrs;
-
-          // Create the like object to be put in the database
-          const likeObject = {
-            statusID: statusID,
-            id: uuid(),
-            liker: liker,
-          };
-
-          // Put Like object in database
-          Like.create(likeObject, (errLike, dataLike) =>  {
-            if (errLike || !dataLike) {
-              callback(null, "There was an error adding like to the table: " + errLike.message);
-            } else {
-              // Increase likesCount by 1
-              statusObject.likesCount = parseInt(statusObject.likesCount, 10) + 1;
-
-              // Update likes count of status in table
-              Status.update(statusObject, (updateErr, updateData) => {
-                if (updateErr || !updateData) {
-                  callback(null, "There was an error updating likes count.");
-                } else{
-                  callback(updateData, null);
-                }
-              });
-            }
-          });
+          callback(null, "Status does not exist.");
         }
-      });
-  }
+      } else {
+        // Status object that is being liked
+        const statusObject = data.Items[0].attrs;
+
+        // Create like object
+        const likeObject = {
+          statusID: statusID,
+          id: uuid(),
+          liker: liker,
+        };
+
+        // Put Like object in table
+        Like.create(likeObject, (errLike, dataLike) =>  {
+          if(errLike || !dataLike) {
+            callback(null, "There was an error adding like to the table: " + errLike.message);
+          } else {
+            // Old Status Object
+            const oldStatus = statusObject;
+
+            // Increase likesCount by 1
+            oldStatus.likesCount = parseInt(oldStatus.likesCount, 10) + 1;
+
+            // Update likes count of status in table
+            Status.update(oldStatus, (updateErr, updateData) => {
+              if(updateErr || !updateData) {
+                callback(null, "There was an error updating likes count.");
+              } else{
+                callback({success: true}, null);
+              }
+            });
+          }
+        });
+      }
+    });
 }
 
 /**
  * Deletes Like object from the likesTable and updates likeCount in the status
  */
 function deleteLike(liker, statusUser, statusID, callback) {
-  if (!liker || !statusUser || !statusID) {
-    callback(null, "Either the liker or status is null.");
-  } else {
-    Status
+  // Query for the status
+  Status
     .query(statusUser)
     .where('id').equals(statusID)
     .exec((err, data) => {
@@ -106,7 +106,7 @@ function deleteLike(liker, statusUser, statusID, callback) {
         // Status object that is being disliked
         const statusObject = data.Items[0].attrs;
 
-        // Find like object and delete it from the likes database
+        // Find like object and delete like
         Like
           .query(statusID)
           .where('liker').equals(liker)
@@ -121,33 +121,29 @@ function deleteLike(liker, statusUser, statusID, callback) {
             } else {
               // Like object for the status by the current liker
               const likeObject = likeData.Items[0].attrs;
-              
-              // Destroy the object in the database
-              Like.destroy(likeObject.statusID, likeObject.liker, (deleteErr) => {
-                if (deleteErr) {
-                  callback(null, "Error trying to delete like: " + deleteErr.message);
-                } else {
-                  // Old status object
-                  const oldStatus = statusObject;
+              Like
+                .destroy(likeObject.statusID, likeObject.liker, (deleteErr) => {
+                  if(deleteErr) {
+                    callback(null, "Error trying to delete like: " + deleteErr.message);
+                  } else {
+                    // Old status object
+                    const oldStatus = statusObject;
 
-                  // Decrement status's likeCount
-                  statusObject.likesCount = parseInt(oldStatus.likesCount, 10) - 1;
-
-                  // Update the status object to reflect the new likes count
-                  Status.update(oldStatus, (updateErr, updateData) => {
-                    if (updateErr || !updateData) {
-                      callback(null, "There was an error updating likes count.");
-                    } else{
-                      callback(updateData, null);
-                    }
-                  });
-                }
-              });
+                    // Decrement status's likeCount
+                    statusObject.likesCount = parseInt(oldStatus.likesCount, 10) - 1;
+                    Status.update(oldStatus, (updateErr, updateData) => {
+                      if(updateErr || !updateData) {
+                        callback(null, "There was an error updating likes count.");
+                      } else{
+                        callback({success: true}, null);
+                      }
+                    });
+                  }
+                });
             }
           });
       }
     });
-  }
 }
 
 // Create an object to store the helper functions
