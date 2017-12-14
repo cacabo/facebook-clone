a0f86087d48541e0c12fd5cd817ec4afd291bd14
ccabo1 <ccabo@seas.upenn.edu>
2017-12-14 02:51:05 -0500

    aggregating all the data

--- backend/mapreduce.js
+++ backend/mapreduce.js
@@ -39,48 +39,111 @@ function getFriends(callback) {
 }
 
 /**
+ * Interest and affiliation helper
+ */
+function affiliationInterestHelper(err, data, callback) {
+  if (err) {
+    // If there is an issue with loading all affiliations
+    callback(null, err.message);
+  } else {
+    // Create a hashmap from affilaitions to arrays of users with that
+    // affiliation
+    const affiliationGroups = {};
+    data.Items.map(item => {
+      const existing = affiliationGroups[item.attrs.affiliation];
+      if (!existing) {
+        affiliationGroups[item.attrs.affiliation] = [item.attrs.username];
+      } else {
+        affiliationGroups[item.attrs.affiliation].push(item.attrs.username);
+      }
+    });
+
+    // Get the list of affiliations
+    const affiliations = Object.keys(affiliationGroups);
+
+    // Construct a map to keep track of all unique (user, user) pairs
+    const userMap = {};
+    affiliations.map(affiliation => {
+      // Get the list of users with the given affiliation
+      const users = affiliationGroups[affiliation];
+      if (users.length > 1) {
+        users.map(user1 => {
+          users.map(user2 => {
+            // Index alphabetically by username
+            if (user1 !== user2) {
+              if (user1 < user2) {
+                userMap[user1 + "\t" + user2] = true;
+              } else {
+                userMap[user2 + "\t" + user1] = true;
+              }
+            }
+          });
+        });
+      }
+    });
+
+    // Reduce all of the pairs into one string
+    const string = Object.keys(userMap).reduce(format, "");
+    callback(string, null);
+  }
+}
+
+/**
  * Function to get all affiliation pairs
  */
 function getAffiliations(callback) {
+  // Load all affiliations
   Affiliation.scan().loadAll().exec((err, data) => {
-    if (err) {
-      callback(null, err.message);
+    affiliationInterestHelper(err, data, callback);
+  });
+}
+
+/**
+ * Function to get all interests pairs
+ */
+function getInterests(callback) {
+  // Load all interests
+  Interest.scan().loadAll().exec((err, data) => {
+    affiliationInterestHelper(err, data, callback);
+  });
+}
+
+/**
+ * Aggregeate the mapreduce data
+ */
+function aggregate(callback) {
+  // Retrive the friend data
+  getFriends((friendData, friendErr) => {
+    if (friendErr) {
+      // If there was an error with the friend data
+      callback(null, friendErr);
     } else {
-      const affiliationGroups = {};
-      data.Items.map(item => {
-        const existing = affiliationGroups[item.attrs.affiliation];
-        if (!existing) {
-          affiliationGroups[item.attrs.affiliation] = [item.attrs.username];
+      // Retrieve the affiliation data
+      getAffiliations((affiliationData, affiliationErr) => {
+        if (affiliationErr) {
+          // If there was an error with the affiliation data
+          callback(null, affiliationErr);
         } else {
-          affiliationGroups[item.attrs.affiliation].push(item.attrs.username);
-        }
-      });
-      const affiliations = Object.keys(affiliationGroups);
-      const userMap = {};
-      affiliations.map(affiliation => {
-        const users = affiliationGroups[affiliation];
-        if (users.length > 1) {
-          users.map(user1 => {
-            users.map(user2 => {
-              if (user1 !== user2) {
-                if (user1 < user2) {
-                  userMap[user1 + "," + user2] = true;
-                } else {
-                  userMap[user2 + "," + user1] = true;
-                }
-              }
-            });
+          // Retrieve all interests data
+          getInterests((interestData, interestErr) => {
+            if (interestErr) {
+              // If there was an error with the interest data
+              callback(null, interestErr);
+            } else {
+              // Aggregate all of the found data into one string
+              const aggregatedData = friendData + affiliationData + interestData;
+              callback(aggregatedData, null);
+            }
           });
         }
       });
-      callback(userMap, null);
     }
   });
 }
 
 // Create an object to store the helper functions
 const mapreduce = {
-  getData: getAffiliations,
+  getData: aggregate,
 };
 
 // Export the object
